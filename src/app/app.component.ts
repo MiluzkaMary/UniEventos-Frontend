a0694ddc43bb6554efb9ae1a12f123ae307c295f
Mary <miluskasaire512@gmail.com>
2024-11-07 17:27:36 -0500

    Primer commit front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterModule} from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'UniEventosFront';
+  footer = 'Universidad del Quindío - 2024-2';
 }
